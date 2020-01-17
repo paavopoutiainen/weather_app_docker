6dd40ba365ae94791a84a5ce2e69cfe9b9811f36
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-01-17 12:41:23 +0200

    first

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || 'b9f72310a5156e47ce945312605c37c7';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
