e7b502e877f8a0c8b3b1419e88b4b8fe726ca205
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-01-18 23:58:19 +0200

    just commiting before changing branch

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const baseURL = "http://localhost:9000/api";
 
-const getWeatherFromApi = async () => {
-  try {
-    const response = await fetch(`${baseURL}/weather`);
-    return response.json();
-  } catch (error) {
-    console.error(error);
-  }
+const Weather = () => {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
 
-  return {};
+  const baseURL = "http://localhost:9000/api";
+
+  const getWeatherFromApi = async city => {
+    if (city) {
+      try {
+        console.log("hello");
+        const response = await fetch(`${baseURL}/weather/${city}`);
+        return response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        const response = await fetch(`${baseURL}/weather`);
+        return response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return {};
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const weather = await getWeatherFromApi(city);
+    setWeather(weather);
+    setCity(null);
+  };
+  const weatherDiv = weather ? <div /> : <div />;
+
+  return (
+    <div>
+      <div>
+        <h2>Weather app</h2>
+        <p>Type in the ciy of which weather you'd like to know</p>
+      </div>
+      <form onSubmit={e => handleSubmit(e)}>
+        Give city:
+        <input
+          type="text"
+          value={city}
+          onChange={e => setCity(e.target.value)}
+        />
+        <button type="submit">search</button>
+      </form>
+      <div className="result">{weatherDiv}</div>
+    </div>
+  );
 };
 
-class Weather extends React.Component {
+/* class Weather extends React.Component {
   constructor(props) {
     super(props);
 
@@ -32,13 +76,9 @@ class Weather extends React.Component {
     const { icon } = this.state;
 
     return (
-      <div>
-        <div>
-          <div className="icon">{icon && <img src={`/img/${icon}.svg`} />}</div>
-        </div>
-      </div>
+      <div className="icon">{icon && <img src={`/img/${icon}.svg`} />}</div>
     );
   }
-}
+} */
 
 ReactDOM.render(<Weather />, document.getElementById("app"));
